2d9c92e1dc04b7d97863e50df0defba541784ac0
Andrey Popov <andpop@mail.ru>
2018-11-22 01:05:43 +0300

    Problem with authorization.

---
+++ main.js
@@ -0,0 +1,19 @@
+console.log('In main.js');
+VK.init({
+    apiId: 6759151
+});
+
+function auth() {
+    return new Promise((resolve, reject) => {
+        VK.Auth.login(data => {
+            if (data.session) {
+                resolve();
+            } else {
+                reject(new Error('Ошибка при авторизации'));
+            }
+        }, 2);
+    });
+}
+
+auth().then(() => console.log('Authorization ok.'));
+// console.log(VK);

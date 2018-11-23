01d7a83b13b04eae83997c2e6cef9a7c0336a45d
Andrey Popov <andpop@mail.ru>
2018-11-24 00:20:38 +0300

    Drug and drop friends.

---
+++ scripts/main.js
@@ -0,0 +1,134 @@
+//Заполнение friendsObj данными о друзьях из ВК
+async function addFriendsFromVK(friendsObj) {
+    try {
+        await auth();
+        const friends = await callAPI('friends.get', { fields: 'photo_50'});
+
+        // console.log(friends);
+        for (const item of friends.items) {
+            if (friendsObj.hasOwnProperty(item.id)) {
+                // Если информация о друге уже была сохранена локально, то обновляем ее из ВК
+                friendsObj[item.id].firstName = item.first_name;
+                friendsObj[item.id].lastName = item.last_name;
+                friendsObj[item.id].photo = item.photo_50;
+            } else {
+                // Если локальной информации о друге не было, то добавляем ее
+                friendsObj[item.id] = {
+                    firstName: item.first_name,
+                    lastName: item.last_name,
+                    photo: item.photo_50,
+                    selected: false
+                };
+            }
+        }
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+function saveFriendsToLocalStorage(friendsObj) {
+    localStorage.friends = JSON.stringify(friendsObj);
+}
+
+function loadFriendsFromLocalStorage() {
+    let data = {};
+
+    if (localStorage.friends) {
+        data = JSON.parse(localStorage.friends);
+    }
+
+    return data;
+}
+
+function toggleFriendStatus(friendsObj, id) {
+    friendsObj[id].selected = !friendsObj[id].selected;
+}
+
+// Все друзья в формате для Handlebars
+function getAllFriendsForHB(friendsObj) {
+    let friendsForHB = [];
+
+    for (let id in friendsObj) {
+        if (friendsObj.hasOwnProperty(id)) {
+            friendsForHB.push({
+                id,
+                firstName: friendsObj[id].firstName,
+                lastName: friendsObj[id].lastName,
+                photo: friendsObj[id].photo,
+                selected: friendsObj[id].selected
+            });
+        }
+    }
+
+    return { items: friendsForHB };
+}
+
+//Невыбранные друзья (левая панель) в формате для Handlebars
+function getUnselectedFriends(friendsObj) {
+    const allFriendsArray = getAllFriendsForHB(friendsObj).items;
+
+    return { items: allFriendsArray.filter(item => !item.selected) };
+}
+
+//Выбранные друзья (правая панель) в формате для Handlebars
+function getSelectedFriends(friendsObj) {
+    const allFriendsArray = getAllFriendsForHB(friendsObj).items;
+
+    return { items: allFriendsArray.filter(item => item.selected) };
+}
+
+function displayUnselectedFriends() {
+    const unselectedFriends = getUnselectedFriends(allFriends);
+    unselectedList.innerHTML = render(unselectedFriends);
+}
+
+function displaySelectedFriends() {
+    const selectedFriends = getSelectedFriends(allFriends);
+    selectedList.innerHTML = render(selectedFriends);
+}
+
+function displayFriends() {
+    displayUnselectedFriends();
+    displaySelectedFriends();
+}
+
+//------------------------------------------------------------------------------------
+
+const
+    selectedContainer = document.querySelector('#selected-container'),
+    selectedList = document.querySelector('#selected-list'),
+    unselectedList = document.querySelector('#unselected-list');
+
+unselectedList.addEventListener('dragstart', e => {
+    event.dataTransfer.setData('id', e.target.dataset.friend_id);
+});
+
+// Делаем контейнер способным принять перемещаемые объекты
+selectedContainer.addEventListener('dragover', e => {
+    e.preventDefault();
+});
+
+selectedContainer.addEventListener('drop', e => {
+    const id = event.dataTransfer.getData('id');
+
+    toggleFriendStatus(allFriends, id);
+    displayFriends();
+});
+
+const friendTemplate = document.querySelector('#friend_template').textContent;
+const render = Handlebars.compile(friendTemplate);
+
+let allFriends = loadFriendsFromLocalStorage();
+
+VK.init({
+    // apiId: 6759151
+    apiId: 6759177
+});
+
+(async () => {
+    await addFriendsFromVK(allFriends);
+    displayFriends();
+
+    // saveFriendsToLocalStorage(allFriends);
+})();
+
